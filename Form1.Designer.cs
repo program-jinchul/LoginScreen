b0b9967e8b89e21725302a9cf9f05bb77e96038e
pjc5767830@gmail.com <pjc5767830@gmail.com>
2026-04-02 11:44:47 +0900

    Login Screen

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,18 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
+            lblAppName = new Label();
+            txtId = new TextBox();
+            txtPw = new TextBox();
+            btnLogin = new Button();
             SuspendLayout();
+            // 
+            // lblAppName
+            // 
+            lblAppName.AutoSize = true;
+            lblAppName.Font = new Font("한컴 고딕", 48F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            lblAppName.ForeColor = Color.FromArgb(255, 128, 0);
+            lblAppName.Location = new Point(215, 45);
+            lblAppName.Name = "lblAppName";
+            lblAppName.Size = new Size(394, 166);
+            lblAppName.TabIndex = 0;
+            lblAppName.Text = "Login";
+            lblAppName.Click += lblAppName_Click;
+            // 
+            // txtId
+            // 
+            txtId.Font = new Font("한컴 고딕", 24F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            txtId.ForeColor = Color.Silver;
+            txtId.Location = new Point(182, 253);
+            txtId.Name = "txtId";
+            txtId.Size = new Size(485, 91);
+            txtId.TabIndex = 2;
+            txtId.Text = "아이디";
+            txtId.TextChanged += txtId_TextChanged;
+            txtId.Enter += txtId_Enter;
+            txtId.Leave += txtId_Leave;
+            // 
+            // txtPw
+            // 
+            txtPw.Font = new Font("한컴 고딕", 24F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            txtPw.ForeColor = Color.Silver;
+            txtPw.Location = new Point(182, 402);
+            txtPw.Name = "txtPw";
+            txtPw.Size = new Size(485, 91);
+            txtPw.TabIndex = 3;
+            txtPw.Text = "패스워드";
+            txtPw.TextChanged += txtPw_TextChanged;
+            txtPw.Enter += txtPw_Enter;
+            txtPw.Leave += txtPw_Leave;
+            // 
+            // btnLogin
+            // 
+            btnLogin.BackColor = Color.Lime;
+            btnLogin.Font = new Font("한컴 고딕", 25.875F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            btnLogin.ForeColor = Color.FromArgb(192, 255, 255);
+            btnLogin.Location = new Point(268, 562);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(320, 130);
+            btnLogin.TabIndex = 1;
+            btnLogin.Text = "로그인";
+            btnLogin.UseVisualStyleBackColor = false;
+            btnLogin.Click += btnLogin_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(14F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(871, 726);
+            ClientSize = new Size(871, 753);
+            Controls.Add(btnLogin);
+            Controls.Add(txtPw);
+            Controls.Add(txtId);
+            Controls.Add(lblAppName);
             Name = "Form1";
-            Text = "Form1";
+            Text = "Login Screen";
+            Load += Form1_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label lblAppName;
+        private TextBox txtId;
+        private TextBox txtPw;
+        private Button btnLogin;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,81 @@ namespace LoginScreen
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtId_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtPw_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblAppName_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtId_Enter(object sender, EventArgs e)
+        {
+            if (txtId.Text == "아이디")
+            {
+                txtId.Text = "";
+
+                txtId.ForeColor = Color.Black;
+            }
+        }
+
+        private void txtId_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtId.Text))
+            {
+
+                txtId.Text = "아이디";
+
+                txtId.ForeColor = Color.Silver;
+            }
+        }
+
+        private void txtPw_Enter(object sender, EventArgs e)
+        {
+            if (txtPw.Text == "패스워드")
+            {
+                txtPw.Text = "";
+                txtPw.ForeColor = Color.Black;
+                txtPw.UseSystemPasswordChar = true;
+            }
+        }
+
+        private void txtPw_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtPw.Text))
+            {
+                txtPw.UseSystemPasswordChar = false;
+                txtPw.Text = "패스워드";
+                txtPw.ForeColor = Color.Silver;
+            }
+        }
+
+        string myID = "admin"; 
+        string myPW = "superman";
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string inputID = txtId.Text;
+            string inputPW = txtPw.Text;
+            
+            if (inputID == myID && inputPW == myPW) { 
+                MessageBox.Show("로그인성공!"); 
+            } 
+            else { 
+                MessageBox.Show("로그인실패~"); 
+            }
+        }
     }
 }
